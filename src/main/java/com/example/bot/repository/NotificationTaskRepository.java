209032b09be6eab10980898988e090015b6ccd86
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-05-25 14:59:11 +0400

    телеграмм бот уведомлений

---
+++ src/main/java/com/example/bot/repository/NotificationTaskRepository.java
@@ -0,0 +1,14 @@
+package com.example.bot.repository;
+
+import com.example.bot.entity.NotificationTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long> {
+
+    List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
+
+
+}
---
+++ src/main/java/com/example/bot/service/NotificationTaskService.java
@@ -0,0 +1,19 @@
+package com.example.bot.service;
+
+import com.example.bot.entity.NotificationTask;
+import com.example.bot.repository.NotificationTaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationTaskService {
+
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public NotificationTaskService(NotificationTaskRepository notificationTaskRepository) {
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
+
+    public void save(NotificationTask notificationTask) {
+        notificationTaskRepository.save(notificationTask);
+    }
+}
